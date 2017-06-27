3deee38ed735394d4754d3d7fa0811641a24542d
ThierryO <Thierry.Onkelinx@inbo.be>
2017-06-27 09:22:26 +0200

    add flanders_slides

--- source/render_all.R
+++ source/render_all.R
@@ -1,4 +1,17 @@
+# main file
 setwd("source")
 rmarkdown::render("index.Rmd", output_dir = "../docs")
+
+# inbo rapport
 setwd("inbo_rapport")
+rmarkdown::render_site(encoding = "UTF-8")
+setwd("../..")
+
+# flanders slides
+source("source/flanders_slides/handout_slides.R")
+file.rename(
+  "docs/flanders_slides/flanders_slides.pdf",
+  "docs/flanders_slides/flanders_slides_handout.pdf"
+)
+setwd("source/flanders_slides")
 rmarkdown::render_site(encoding = "UTF-8")
