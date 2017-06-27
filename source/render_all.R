31e9c1025dd801b0a3e738c05524b6677f600e67
ThierryO <Thierry.Onkelinx@inbo.be>
2017-06-27 09:57:55 +0200

    render inbo_slides

--- source/render_all.R
+++ source/render_all.R
@@ -15,3 +15,19 @@ file.rename(
 )
 setwd("source/flanders_slides")
 rmarkdown::render_site(encoding = "UTF-8")
+setwd("../..")
+
+# inbo slides
+source("source/inbo_slides/handout_slides.R")
+file.rename(
+  "docs/inbo_slides/inbo_slides.pdf",
+  "docs/inbo_slides/inbo_slides_handout.pdf"
+)
+source("source/inbo_slides/handout_rapport.R")
+file.rename(
+  "docs/inbo_slides/inbo_slides.pdf",
+  "docs/inbo_slides/inbo_slides_rapport.pdf"
+)
+setwd("source/inbo_slides")
+rmarkdown::render_site(encoding = "UTF-8")
+setwd("../..")
