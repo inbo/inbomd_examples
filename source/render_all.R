fec39e57d4a4aa82293b88c192ca964aee46be54
ThierryO <Thierry.Onkelinx@inbo.be>
2017-06-29 14:49:26 +0200

    bugfix: set correct filenames and working directories

--- source/render_all.R
+++ source/render_all.R
@@ -5,29 +5,27 @@ rmarkdown::render("index.Rmd", output_dir = "../docs")
 # inbo rapport
 setwd("inbo_rapport")
 rmarkdown::render_site(encoding = "UTF-8")
-setwd("../..")
 
 # flanders slides
-source("source/flanders_slides/handout_slides.R")
+setwd("../flanders_slides")
+source("handout_slides.R")
 file.rename(
-  "docs/flanders_slides/flanders_slides.pdf",
-  "docs/flanders_slides/flanders_slides_handout.pdf"
+  "../../docs/flanders_slides/flanders_slides.pdf",
+  "../../docs/flanders_slides/flanders_slides_handout.pdf"
 )
-setwd("source/flanders_slides")
 rmarkdown::render_site(encoding = "UTF-8")
-setwd("../..")
 
 # inbo slides
-source("source/inbo_slides/handout_slides.R")
+setwd("../inbo_slides")
+source("handout_slides.R")
 file.rename(
-  "docs/inbo_slides/inbo_slides.pdf",
-  "docs/inbo_slides/inbo_slides_handout.pdf"
+  "../../docs/inbo_slides/inbo_slides.pdf",
+  "../../docs/inbo_slides/inbo_slides_handout.pdf"
 )
-source("source/inbo_slides/handout_rapport.R")
+source("handout_rapport.R")
 file.rename(
-  "docs/inbo_slides/inbo_slides.pdf",
-  "docs/inbo_slides/inbo_slides_rapport.pdf"
+  "../../docs/inbo_slides/inbo_slides.pdf",
+  "../../docs/inbo_slides/inbo_slides_rapport.pdf"
 )
-setwd("source/inbo_slides")
 rmarkdown::render_site(encoding = "UTF-8")
 setwd("../..")
